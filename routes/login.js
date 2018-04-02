7ac20e1f813260e47892ee7bd0b1bdf3382dcd2e
zhengxu <332171564@qq.com>
2018-04-03 01:24:20 +0800

    动态修改密码

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,12 @@
+var MD5 = require("crypto-js/md5");
 const
     db = require('../module/db')
 
 
-
 const login = (req, res) => {
+    // MD5加密判断
+    req.body.password = MD5(req.body.password).toString()
+
     db.find('users', req.body).then(({ data }) => {
         if (data.length == 0) {
             res.send(JSON.stringify({
---
+++ scripts/changePassword.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: zhengxu
+ * @Date: 2018-04-03 00:36:03
+ * @Last Modified by: zhengxu
+ * @Last Modified time: 2018-04-03 01:19:47
+ * @describe 简单地直接调用这个文件修改密码
+ */
+
+
+const db = require('../module/db')
+minimist = require('minimist')
+MD5 = require("crypto-js/md5");
+
+var args = minimist(process.argv.slice(2)); // {user:xxx,password:xxx}
+
+if (args.user && args.password) {
+    db.update('users', { user: args.user }, { $set: { password: MD5(args.password.toString()).toString() } }) // md5加密
+        .then((data) => {
+            data.result.n > 0 ? console.log('成功') : console.log('失败:' + data.results);
+        })
+        .catch(err => console.log(err))
+} else {
+    console.log('缺失参数');
+}
